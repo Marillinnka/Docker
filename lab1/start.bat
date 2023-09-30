2acbe3a892bfb505f95be7ee5f801fd81b63ca96
Makarova Marina <marina25032002@mail.ru>
2023-09-30 15:48:54 +0300

    Исправила замечания по 1 ЛР

--- lab1/start.bat
+++ lab1/start.bat
@@ -1,4 +1,4 @@
 docker build -t 2048-game .
 
-docker run -d -P --name first-container 2048-game
-docker run -d -P --name second-container 2048-game
+docker run -d -p 1050:8080 --name first-container 2048-game
+docker run -d -p 2453:8080 --name second-container 2048-game
